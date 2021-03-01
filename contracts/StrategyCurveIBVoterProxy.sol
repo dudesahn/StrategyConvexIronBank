1a5d65a9b459d719fa5592d627829374ec6d5305
dudesahn <dudesahn@gmail.com>
2021-02-28 19:32:16 -0500

    voter upgradeable

--- contracts/StrategyCurveIBVoterProxy.sol
+++ contracts/StrategyCurveIBVoterProxy.sol
@@ -21,7 +21,7 @@ contract StrategyCurveIBVoterProxy is BaseStrategy {
     address public crvRouter = 0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F; // default to sushiswap
 
     address public constant crvIBgauge = address(0xF5194c3325202F456c95c1Cf0cA36f8475C1949F); // Curve Iron Bank Gauge contract, v2 is tokenized, held by curveProxy
-    address public constant voter = address(0xF147b8125d2ef93FB6965Db97D6746952a133934); // Yearn's veCRV voter
+    address public voter = address(0xF147b8125d2ef93FB6965Db97D6746952a133934); // Yearn's veCRV voter
 
     address[] public crvPath;
     address[] internal crvPathDai;
