481a09651f038ab3360e2fb42d21dd1b64883475
dudesahn <dudesahn@gmail.com>
2021-03-05 16:04:30 -0500

    more tests updates

--- tests/test_emergency_exit.py
+++ tests/test_emergency_exit.py
@@ -5,11 +5,11 @@ def test_emergency_exit(accounts, token, vault, strategy, strategist, amount):
     # Deposit to the vault
     token.approve(vault.address, amount, {"from": accounts[0]})
     vault.deposit(amount, {"from": accounts[0]})
-    strategy.setOptimal(0)
-    strategy.harvest()
+    strategy.setOptimal(0, {'from': strategist})
+    strategy.harvest({'from': strategist})
     assert token.balanceOf(strategy.address) == amount
 
     # set emergency and exit
     strategy.setEmergencyExit()
-    strategy.harvest()
+    strategy.harvest({'from': strategist})
     assert token.balanceOf(strategy.address) < amount
