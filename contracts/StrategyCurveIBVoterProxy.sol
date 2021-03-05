8ff28d8f9571563ca618ae3d7ad69e416e789887
dudesahn <dudesahn@gmail.com>
2021-03-04 19:22:33 -0500

    Update StrategyCurveIBVoterProxy.sol

--- contracts/StrategyCurveIBVoterProxy.sol
+++ contracts/StrategyCurveIBVoterProxy.sol
@@ -2,13 +2,15 @@
 pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
 
-import {BaseStrategy} from "@yearnvaults/contracts/BaseStrategy.sol";
+// These are the core Yearn libraries
 import {
-    SafeERC20,
-    SafeMath,
-    IERC20,
-    Address
-} from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+    BaseStrategy
+} from "@yearnvaults/contracts/BaseStrategy.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/math/Math.sol";
 
 import "./interfaces/curve.sol";
 import "./interfaces/yearn.sol";
