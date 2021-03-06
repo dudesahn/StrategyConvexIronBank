465b3473a64af83390483a2826382f5d9233ffd2
dudesahn <dudesahn@gmail.com>
2021-03-05 23:00:59 -0500

    Update test_emergency_exit.py

--- tests/test_emergency_exit.py
+++ tests/test_emergency_exit.py
@@ -2,14 +2,14 @@ import brownie
 from brownie import Contract
 
 def test_emergency_exit(accounts, token, vault, strategy, strategist, amount, whale, curve_proxy, gauge):
-    # Deposit to the vault
+    # Deposit to the vault, confirm that funds are in the gauge
     token.approve(vault.address, amount, {"from": whale})
     vault.deposit(amount, {"from": whale})
     strategy.setOptimal(0)
     strategy.harvest({"from": strategist})
     assert curve_proxy.balanceOf(gauge) == amount
 
-    # set emergency and exit, then confirm that the 
+    # set emergency and exit, then confirm that the strategy has no funds
     strategy.setEmergencyExit()
     strategy.harvest({"from": strategist})
     assert strategy.estimatedTotalAssets() == 0
