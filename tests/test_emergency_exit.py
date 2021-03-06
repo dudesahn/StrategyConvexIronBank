66819871d065989813fb5f0114e0f0b45efdd2e1
dudesahn <dudesahn@gmail.com>
2021-03-05 22:49:37 -0500

    Update test_emergency_exit.py

--- tests/test_emergency_exit.py
+++ tests/test_emergency_exit.py
@@ -4,7 +4,7 @@ from brownie import Contract
 def test_emergency_exit(accounts, token, vault, strategy, strategist, amount, whale):
     # Deposit to the vault
     token.approve(vault.address, amount, {"from": whale})
-    vault.deposit(amount, {"from": gov})
+    vault.deposit(amount, {"from": whale})
     strategy.setOptimal(0)
     strategy.harvest({"from": strategist})
     assert token.balanceOf(strategy.address) == amount
