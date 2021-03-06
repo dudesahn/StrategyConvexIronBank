28abc562f51e9e835c85044098bededc75950d13
dudesahn <dudesahn@gmail.com>
2021-03-05 22:48:37 -0500

    update tests

--- tests/test_emergency_exit.py
+++ tests/test_emergency_exit.py
@@ -1,9 +1,9 @@
 import brownie
 from brownie import Contract
 
-def test_emergency_exit(accounts, token, vault, strategy, strategist, amount, gov):
+def test_emergency_exit(accounts, token, vault, strategy, strategist, amount, whale):
     # Deposit to the vault
-    token.approve(vault.address, amount, {"from": gov})
+    token.approve(vault.address, amount, {"from": whale})
     vault.deposit(amount, {"from": gov})
     strategy.setOptimal(0)
     strategy.harvest({"from": strategist})
