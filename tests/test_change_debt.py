1cfeccebfe6d5baa32106e2f0287c8c82a36461c
dudesahn <dudesahn@gmail.com>
2021-03-06 13:28:40 -0500

    Update test_change_debt.py

--- tests/test_change_debt.py
+++ tests/test_change_debt.py
@@ -4,21 +4,19 @@ from brownie import Contract
 def test_change_debt(gov, token, vault, strategy, strategist, whale, strategyProxy, gauge):
     # Deposit to the vault and harvest
     token.approve(vault.address, 10_000, {"from": whale})
-    vault.deposit(10_000, {"from": whale})
-    vault.updateStrategyDebtRatio(strategy.address, 5_000, {"from": gov})
+    vault.deposit(10000000000000000000, {"from": whale})
+    vault.updateStrategyDebtRatio(strategy.address, 5000000000000000000, {"from": gov})
     strategy.setCrvRouter(0)
     strategy.setOptimal(0)
     strategy.harvest({"from": strategist})
 
-    assert strategyProxy.balanceOf(gauge) == 5_000
+    assert strategyProxy.balanceOf(gauge) == 5000000000000000000
 
-    vault.updateStrategyDebtRatio(strategy.address, 10_000, {"from": gov})
+    vault.updateStrategyDebtRatio(strategy.address, 10000000000000000000, {"from": gov})
     strategy.harvest({"from": strategist})
-    assert strategyProxy.balanceOf(gauge) == 10_000
+    assert strategyProxy.balanceOf(gauge) == 10000000000000000000
 
     # In order to pass this tests, you will need to implement prepareReturn.
     # TODO: uncomment the following lines.
     # vault.updateStrategyDebtRatio(strategy.address, 5_000, {"from": gov})
     # assert token.balanceOf(strategy.address) == amount / 2
-
-
