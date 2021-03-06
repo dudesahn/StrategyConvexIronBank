e727633585fd4af8384f71f1dc2629191d205417
dudesahn <dudesahn@gmail.com>
2021-03-06 17:01:09 -0500

    Update test_revoke.py

--- tests/test_revoke.py
+++ tests/test_revoke.py
@@ -15,39 +15,41 @@ def test_revoke_strategy_from_vault(token, vault, strategy, amount, gov, strateg
     old_vault_balance = token.balanceOf(vault)
 
     vault.revokeStrategy(strategy.address, {"from": gov})
+    assert strategy.estimatedTotalAssets() == amount
+    assert token.balanceOf(vault) == 0
     strategy.harvest({"from": strategist})
     
-#     new_assets_dai = vault.totalAssets()
-#     new_proxy_balanceOf_gauge = strategyProxy.balanceOf(gauge)
-#     new_gauge_balanceOf_voter = gauge.balanceOf(voter)
-#     new_strategy_balance = token.balanceOf(strategy)
-#     new_estimated_total_assets = strategy.estimatedTotalAssets()
-#     new_vault_balance = token.balanceOf(vault)
-#     
-#     # Check for any assets only in the vault, not in the strategy
-#     print("\nOld Vault Holdings: ", old_vault_balance)
-#     print("\nNew Vault Holdings: ", new_vault_balance)  
-#     
-#     # Check total assets in the strategy
-#     print("\nOld Strategy totalAssets: ", old_estimated_total_assets)
-#     print("\nNew Strategy totalAssets: ", new_estimated_total_assets)  
-#     
-#     # Check total assets in the vault + strategy
-#     print("\nOld Vault totalAssets: ", old_assets_dai)
-#     print("\nNew Vault totalAssets: ", new_assets_dai)
-#     
-#     # Want token should never be in the strategy    
-#     print("\nOld Strategy balanceOf: ", old_strategy_balance)
-#     print("\nNew Strategy balanceOf: ", new_strategy_balance)
-#     
-#     # These two calls should return the same value, and should update after every harvest call
-#     print("\nOld Proxy balanceOf gauge: ", old_proxy_balanceOf_gauge)
-#     print("\nNew Proxy balanceOf gauge: ", new_proxy_balanceOf_gauge)
-#     print("\nOld gauge balanceOf voter: ", old_gauge_balanceOf_voter)
-#     print("\nNew gauge balanceOf voter: ", new_gauge_balanceOf_voter)
+    new_assets_dai = vault.totalAssets()
+    new_proxy_balanceOf_gauge = strategyProxy.balanceOf(gauge)
+    new_gauge_balanceOf_voter = gauge.balanceOf(voter)
+    new_strategy_balance = token.balanceOf(strategy)
+    new_estimated_total_assets = strategy.estimatedTotalAssets()
+    new_vault_balance = token.balanceOf(vault)
+    
+    # Check for any assets only in the vault, not in the strategy
+    print("\nOld Vault Holdings: ", old_vault_balance)
+    print("\nNew Vault Holdings: ", new_vault_balance)  
+    
+    # Check total assets in the strategy
+    print("\nOld Strategy totalAssets: ", old_estimated_total_assets)
+    print("\nNew Strategy totalAssets: ", new_estimated_total_assets)  
+    
+    # Check total assets in the vault + strategy
+    print("\nOld Vault totalAssets: ", old_assets_dai)
+    print("\nNew Vault totalAssets: ", new_assets_dai)
+    
+    # Want token should never be in the strategy    
+    print("\nOld Strategy balanceOf: ", old_strategy_balance)
+    print("\nNew Strategy balanceOf: ", new_strategy_balance)
+    
+    # These two calls should return the same value, and should update after every harvest call
+    print("\nOld Proxy balanceOf gauge: ", old_proxy_balanceOf_gauge)
+    print("\nNew Proxy balanceOf gauge: ", new_proxy_balanceOf_gauge)
+    print("\nOld gauge balanceOf voter: ", old_gauge_balanceOf_voter)
+    print("\nNew gauge balanceOf voter: ", new_gauge_balanceOf_voter)
     
     
-    assert token.balanceOf(vault) >= amount
+    assert token.balanceOf(vault) == amount
 
 
 def test_revoke_strategy_from_strategy(token, vault, strategy, amount, strategist, whale, gov):
@@ -62,5 +64,4 @@ def test_revoke_strategy_from_strategy(token, vault, strategy, amount, strategis
     strategy.setEmergencyExit({"from": gov})
     strategy.harvest({"from": strategist})
     assert token.balanceOf(vault) == amount
-    
     
