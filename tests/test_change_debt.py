2e40837e3b662dc1ed842d33c1089d1da5d12534
dudesahn <dudesahn@gmail.com>
2021-03-06 13:11:04 -0500

    Update test_change_debt.py

--- tests/test_change_debt.py
+++ tests/test_change_debt.py
@@ -5,13 +5,13 @@ def test_change_debt(gov, token, vault, strategy, strategist, amount, whale):
     # Deposit to the vault and harvest
     token.approve(vault.address, amount, {"from": whale})
     vault.deposit(amount, {"from": whale})
-    half = amount / 2
+    half = 100000000000
     vault.updateStrategyDebtRatio(strategy.address, half, {"from": gov})
     strategy.setCrvRouter(0)
     strategy.setOptimal(0)
     strategy.harvest({"from": strategist})
 
-    assert token.balanceOf(strategy.address) == amount / 2
+    assert token.balanceOf(strategy.address) == half
 
     vault.updateStrategyDebtRatio(strategy.address, amount, {"from": gov})
     strategy.harvest({"from": strategist})
