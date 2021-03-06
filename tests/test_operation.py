fdec09e1147fd85b2c77a05869f8cc6e4a3ae3b2
dudesahn <dudesahn@gmail.com>
2021-03-06 09:10:34 -0500

    Update test_operation.py

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,6 +1,8 @@
 import brownie
 # from helpers import showBalances
 from brownie import Contract
+# from useful_methods import genericStateOfStrat,genericStateOfVault
+
 
 # **** TEST ALL CONTRACT FUNCTIONS
 
@@ -16,7 +18,7 @@ def test_operation(token, vault, strategy, strategist, amount, whale, gauge, cur
 
     # harvest, store asset amount
     strategy.harvest({"from": strategist})
-    old_assets = vault.totalAssets()
+    old_assets_dai = vault.totalAssets()
     assert curve_proxy.balanceOf(gauge) == amount
 
     # simulate a month of earnings
@@ -25,33 +27,48 @@ def test_operation(token, vault, strategy, strategist, amount, whale, gauge, cur
 
     # harvest after a month, store new asset amount
     strategy.harvest({"from": strategist})
-    new_assets = vault.totalAssets()
+    new_assets_dai = vault.totalAssets()
     assert curve_proxy.balanceOf(gauge) > amount
+    assert new_assets_dai > old_assets_dai
     
-    
-    genericStateOfStrat(strategy, currency, vault)
-    genericStateOfVault(vault, currency)
+#     genericStateOfStrat(strategy, currency, vault)
+#     genericStateOfVault(vault, currency)
 
     # Display estimated APR based on the past month
-    print("\nEstimated APR: ", "{:.2%}".format(((vault.totalAssets()-assets)*12)/(assets)))
+    print("\nEstimated DAI APR: ", "{:.2%}".format(((new_assets_dai-old_assets_dai)*12)/(old_assets_dai)))
     
     
-    
+    # set optimal to USDC. new_assets_dai is now our new baseline
+    strategy.setOptimal(1)
 
+    # simulate a month of earnings
+    chain.sleep(2592000)
+    chain.mine(1)
 
-#     set optimal to USDC
-#     strategy.setOptimal(1)
-# 
-#     harvest
-#     strategy.harvest({"from": strategist})
-#     assert token.balanceOf(strategy.address) == amount
-# 
-#     set optimal to USDT
-#     strategy.setOptimal(2)
-# 
-#     harvest
-#     strategy.harvest({"from": strategist})
-#     assert token.balanceOf(strategy.address) == amount
+    # harvest after a month, store new asset amount after switch to USDC
+    strategy.harvest({"from": strategist})
+    new_assets_usdc = vault.totalAssets()
+    assert curve_proxy.balanceOf(gauge) > amount
+    assert new_assets_usdc > new_assets_dai
+
+    # Display estimated APR based on the past month
+    print("\nEstimated USDC APR: ", "{:.2%}".format(((new_assets_usdc-new_assets_dai)*12)/(new_assets_dai)))
+
+    # set optimal to USDT, new_assets_usdc is now our new baseline
+    strategy.setOptimal(2)
+    
+    # simulate a month of earnings
+    chain.sleep(2592000)
+    chain.mine(1)
+
+    # harvest after a month, store new asset amount
+    strategy.harvest({"from": strategist})
+    new_assets_usdt = vault.totalAssets()
+    assert curve_proxy.balanceOf(gauge) > amount
+    assert new_assets_usdt > new_assets_usdc
+    
+    # Display estimated APR based on the past month
+    print("\nEstimated USDT APR: ", "{:.2%}".format(((new_assets_usdt-new_assets_usdc)*12)/(new_assets_usdc)))
 
     # tend()
     strategy.tend()
