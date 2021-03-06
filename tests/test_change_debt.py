d17d490a7e666bb4dffd066360fea03924158d8c
dudesahn <dudesahn@gmail.com>
2021-03-06 13:34:08 -0500

    Update test_change_debt.py

--- tests/test_change_debt.py
+++ tests/test_change_debt.py
@@ -5,7 +5,9 @@ def test_change_debt(gov, token, vault, strategy, strategist, whale, strategyPro
     # Deposit to the vault and harvest
     token.approve(vault.address, 10000000000000000000, {"from": whale})
     vault.deposit(10000000000000000000, {"from": whale})
-    vault.updateStrategyDebtRatio(strategy, 5000000000000000000, {"from": gov})
+    
+    # debtRatio is in BPS (aka, max is 10,000, which represents 100%), and is a fraction of the funds that can be in the strategy
+    vault.updateStrategyDebtRatio(strategy, 5000, {"from": gov})
     strategy.setCrvRouter(0)
     strategy.setOptimal(0)
     strategy.harvest({"from": strategist})
