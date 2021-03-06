267799ced454fb82eaa14d54fcd7e17ab07088a6
dudesahn <dudesahn@gmail.com>
2021-03-06 10:41:13 -0500

    Update test_simple_harvest.py

--- tests/test_simple_harvest.py
+++ tests/test_simple_harvest.py
@@ -18,6 +18,19 @@ def test_operation(token, vault, strategy, strategist, amount, whale, gauge, str
     assert old_assets_dai == amount
     assert old_assets_dai == strategyProxy.balanceOf(gauge)
 
+    # simulate a month of earnings
+    chain.sleep(2592000)
+    chain.mine(1)
+
+    # harvest after a month, store new asset amount
+    strategy.harvest({"from": strategist})
+    new_assets_dai = vault.totalAssets()
+    assert new_assets_dai > old_assets_dai
+    
+    # Display estimated APR based on the past month
+    print("\nEstimated DAI APR: ", "{:.2%}".format(((new_assets_dai-old_assets_dai)*12)/(old_assets_dai)))
+
+
     # tend()
     strategy.tend()
 
