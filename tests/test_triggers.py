f889c73c550c2754d4b8f856f669ad8335ddde88
dudesahn <dudesahn@gmail.com>
2021-03-05 21:57:22 -0500

    update testing

--- tests/test_triggers.py
+++ tests/test_triggers.py
@@ -3,7 +3,7 @@ from brownie import Contract
 
 def test_triggers(gov, vault, strategy, token, amount, strategist):
     # Deposit to the vault and harvest
-    token.approve(vault.address, amount, {"from": gov})
+    token.approve(vault.address, amount, {"from": whale})
     vault.deposit(amount, {"from": gov})
     vault.updateStrategyDebtRatio(strategy.address, 5_000, {"from": gov})
     strategy.setOptimal(0)
