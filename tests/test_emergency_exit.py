d8944f5ecdb09eb3d4eaf0375727903a2573ac9d
dudesahn <dudesahn@gmail.com>
2021-03-08 15:15:44 -0500

    Update test_emergency_exit.py

--- tests/test_emergency_exit.py
+++ tests/test_emergency_exit.py
@@ -1,28 +1,22 @@
 import brownie
 from brownie import Contract
 import pytest
-from brownie import config
-
-
-@pytest.fixture
-def reserve(accounts):
-    # this is the gauge contract, holds >99% of pool tokens. use this to seed our whale, as well for calling functions above as gauge
-    yield accounts.at("0xF5194c3325202F456c95c1Cf0cA36f8475C1949F", force=True)         
+from brownie import config        
 
 @pytest.fixture
-def whale(accounts, token ,reserve):
+def whale_emergency(accounts, token ,reserve):
     # Totally in it for the tech
-    # Has 10% of tokens (was in the ICO)
+    # Has 5% of tokens (was in the ICO)
     a = accounts[6]
-    bal = token.totalSupply() // 10
+    bal = token.totalSupply() // 20
     token.transfer(a, bal, {"from":reserve})
     yield a
 
 
-def test_emergency_exit(accounts, token, vault, strategy, strategist, amount, whale, strategyProxy, gaugeIB):
+def test_emergency_exit(accounts, token, vault, strategy, strategist, amount, whale_emergency, strategyProxy, gaugeIB):
     # Deposit to the vault, confirm that funds are in the gauge
-    token.approve(vault.address, amount, {"from": whale})
-    vault.deposit(amount, {"from": whale})
+    token.approve(vault.address, amount, {"from": whale_emergency})
+    vault.deposit(amount, {"from": whale_emergency})
     strategy.setCrvRouter(0)
     strategy.setOptimal(0)
     strategy.harvest({"from": strategist})
