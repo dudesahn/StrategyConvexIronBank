c8a0aeaf76d5af9a5cece858322b74615ccdb27b
dudesahn <dudesahn@gmail.com>
2021-03-10 22:15:40 -0500

    Update test_setters.py

--- tests/test_setters.py
+++ tests/test_setters.py
@@ -2,7 +2,7 @@ import brownie
 from brownie import Contract
 from brownie import config
 
-def test_setters(token, vault, strategy, strategist, whale, gaugeIB, strategyProxy, chain, voter, new_address):
+def test_setters(token, vault, strategy, strategist, whale, gaugeIB, strategyProxy, chain, voter, new_address, gov):
 	# Test all other setter functions, using this address: 0x6b3595068778dd592e39a122f4f5a5cf09c90fe2
 	
 	strategy.setProxy(new_address, {"from": gov})
