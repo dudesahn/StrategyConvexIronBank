617099ec86a7fa70c8c5c53f9851611bb447beda
dudesahn <dudesahn@gmail.com>
2021-03-11 22:41:28 -0500

    update to previous versions of interfaces

--- contracts/interfaces/curve.sol
+++ contracts/interfaces/curve.sol
@@ -5,20 +5,76 @@ pragma experimental ABIEncoderV2;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
 interface IGauge {
+    function deposit(uint256) external;
+
     function balanceOf(address) external view returns (uint256);
+
+    function claim_rewards() external;
+
+    function claimable_tokens(address) external view returns (uint256);
+
+    function claimable_reward(address _addressToCheck, address _rewardToken)
+        external
+        view
+        returns (uint256);
 
     function withdraw(uint256) external;
 }
 
 interface ICurveFi {
+    function get_virtual_price() external view returns (uint256);
+
     function add_liquidity(
         // Iron bank pool
         uint256[3] calldata amounts,
         uint256 min_mint_amount,
         bool use_underlying
     ) external payable;
+
+    function remove_liquidity_imbalance(
+        uint256[2] calldata amounts,
+        uint256 max_burn_amount
+    ) external;
+
+    function remove_liquidity(uint256 _amount, uint256[2] calldata amounts)
+        external;
+
+    function remove_liquidity_one_coin(
+        uint256 _token_amount,
+        int128 i,
+        uint256 min_amount
+    ) external;
+
+    function exchange(
+        int128 from,
+        int128 to,
+        uint256 _from_amount,
+        uint256 _min_to_amount
+    ) external;
+
+    function balances(uint256) external view returns (uint256);
+
+    function get_dy(
+        int128 from,
+        int128 to,
+        uint256 _from_amount
+    ) external view returns (uint256);
+
+    function calc_token_amount(uint256[2] calldata amounts)
+        external
+        view
+        returns (uint256);
+
+    function calc_withdraw_one_coin(uint256 amount, int128 i)
+        external
+        view
+        returns (uint256);
 }
 
 interface ICrvV3 is IERC20 {
     function minter() external view returns (address);
+}
+
+interface IMinter {
+    function mint(address) external;
 }
--- contracts/interfaces/yearn.sol
+++ contracts/interfaces/yearn.sol
@@ -2,6 +2,8 @@
 pragma solidity >=0.6.0 <0.7.0;
 pragma experimental ABIEncoderV2;
 
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+
 interface ICurveStrategyProxy {
     function approveStrategy(address _gauge, address _strategy) external;
 
@@ -17,11 +19,31 @@ interface ICurveStrategyProxy {
 
     function balanceOf(address _gauge) external view returns (uint256);
 
+    //        return IERC20(_gauge).balanceOf(address(proxy));
+
     function withdrawAll(address _gauge, address _token)
         external
         returns (uint256);
 
     function deposit(address _gauge, address _token) external;
 
+    //        require(strategies[_gauge] == msg.sender, "!strategy");
+    //        uint _balance = IERC20(_token).balanceOf(address(this));
+    //        IERC20(_token).safeTransfer(address(proxy), _balance);
+    //        _balance = IERC20(_token).balanceOf(address(proxy));
+    //
+    //        proxy.execute(_token, 0, abi.encodeWithSignature("approve(address,uint256)", _gauge, 0));
+    //        proxy.execute(_token, 0, abi.encodeWithSignature("approve(address,uint256)", _gauge, _balance));
+    //        (bool success, ) = proxy.execute(_gauge, 0, abi.encodeWithSignature("deposit(uint256)", _balance));
+    //        if (!success) assert(false);
+
     function harvest(address _gauge) external;
+
+    //        require(strategies[_gauge] == msg.sender, "!strategy");
+    //        uint _balance = IERC20(crv).balanceOf(address(proxy));
+    //        proxy.execute(mintr, 0, abi.encodeWithSignature("mint(address)", _gauge));
+    //        _balance = (IERC20(crv).balanceOf(address(proxy))).sub(_balance);
+    //        proxy.execute(crv, 0, abi.encodeWithSignature("transfer(address,uint256)", msg.sender, _balance));
+
+    function claim(address recipient) external;
 }
