ff6a6a1c2698064ffd10687aba35b4ffbc18e719
dudesahn <dudesahn@gmail.com>
2021-05-18 18:48:32 -0400

    feat: test updates

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,6 +19,9 @@ def token():
 def crv():
     yield Contract("0xD533a949740bb3306d119CC777fa900bA034cd52")
 
+@pytest.fixture
+def cvx():
+    yield Contract("0xD533a949740bb3306d119CC777fa900bA034cd52")
 
 @pytest.fixture
 def dai():
@@ -36,147 +39,76 @@ def gaugeIB():
     # this is the gauge contract for the Iron Bank Curve Pool, in Curve v2 these are tokenized.
     yield Contract("0xF5194c3325202F456c95c1Cf0cA36f8475C1949F")
 
-
 # Define any accounts in this section
-
-
 @pytest.fixture
 def gov(accounts):
     # yearn multis... I mean YFI governance. I swear!
     yield accounts.at("0xFEB4acf3df3cDEA7399794D0869ef76A6EfAff52", force=True)
 
-
 @pytest.fixture
 def dudesahn(accounts):
-    yield accounts.at("0x677Ae1C4FDa1A986a23a055Bbd0A94f8e5b284De", force=True)
-
-@pytest.fixture
-def vault_balance(accounts):
-    yield accounts.at("0x27b7b1ad7288079A66d12350c828D3C00A6F07d7", force=True)
-
-@pytest.fixture
-def rewards(accounts):
-    yield accounts[0]
-
-@pytest.fixture
-def guardian(accounts):
-    yield accounts[1]
-
-
-@pytest.fixture
-def management(accounts):
-    yield accounts[2]
-
-
-@pytest.fixture
-def strategist(accounts):
-    yield accounts[3]
-
-
-@pytest.fixture
-def keeper(accounts):
-    yield accounts[4]
-
-
-@pytest.fixture
-def rando(accounts):
-    yield accounts[5]
+    yield accounts.at("0x8Ef63b525fceF7f8662D98F77f5C9A86ae7dFE09", force=True)
 
 @pytest.fixture
 def strategist_ms(accounts):
     # like governance, but better
     yield accounts.at("0x16388463d60FFE0661Cf7F1f31a7D658aC790ff7", force=True)
 
-
 @pytest.fixture
-def reserve(accounts):
-    # this is the gauge contract, holds >99% of pool tokens. use this to seed our whale, as well for calling functions above as gauge
-    yield accounts.at("0xF5194c3325202F456c95c1Cf0cA36f8475C1949F", force=True)
+def keeper(accounts):
+    yield accounts[0]
 
 
 @pytest.fixture
-def new_address(accounts):
-    # new account for voter and proxy tests
-    yield accounts.at("0xb5DC07e23308ec663E743B1196F5a5569E4E0555", force=True)
-
-
-# Whale accounts
-@pytest.fixture
-def whale(accounts, token, reserve):
-    # Totally in it for the tech
-    acc = accounts.at('0xF5194c3325202F456c95c1Cf0cA36f8475C1949F', force=True)
-    yield acc
-
-# Define the amount of tokens that our whale will be using
-@pytest.fixture
-def amount(accounts, token, whale):
-    amount = Wei('1000 ether')
-    # In order to get some funds for the token you are about to use,
-    # it impersonate an exchange address to use it's funds.
-    reserve = whale
-    token.transfer(accounts[7], amount, {"from": reserve})
-    yield amount
+def rewards(accounts):
+    yield accounts[1]
 
 
 @pytest.fixture
-def strategyProxy(interface):
-    # This is Yearn's StrategyProxy contract, overlord of the Curve world
-    yield interface.ICurveStrategyProxy("0x9a165622a744C20E3B2CB443AeD98110a33a231b")
+def guardian(accounts):
+    yield accounts[2]
 
 
-# @pytest.fixture
-# def vault(pm, gov, rewards, guardian, management, token):
-#     Vault = pm(config["dependencies"][0]).Vault
-#     vault = guardian.deploy(Vault)
-#     vault.initialize(token, gov, rewards, "", "", guardian)
-#     vault.setDepositLimit(2 ** 256 - 1, {"from": gov})
-#     vault.setManagement(management, {"from": gov})
-#     yield vault
+@pytest.fixture
+def management(accounts):
+    yield accounts[3]
+
+
+@pytest.fixture
+def strategist(accounts):
+    yield accounts[4]
+
+@pytest.fixture
+def strategist_ms(accounts):
+    # like governance, but better
+    yield accounts.at("0x16388463d60FFE0661Cf7F1f31a7D658aC790ff7", force=True)
+
+@pytest.fixture
+def whale(accounts):
+    # Totally in it for the tech (largest EOA holder of ib-3crv, ~600k worth)
+    whale = accounts.at('0xE594173Aaa1493665EC6A19a0D170C76EEa1124a', force=True)
+    yield whale
+
+# this is the live strategy for ib3crv
+@pytest.fixture
+def curveVoterProxyStrategy():
+    yield Contract("0x5148C3124B42e73CA4e15EEd1B304DB59E0F2AF7")
+
+@pytest.fixture
+def strategy(strategist, keeper, vault, StrategyConvexCurveLP, gov, curveVoterProxyStrategy, guardian, liveGov):
+	# parameters for this are: strategy, vault, max deposit, minTimePerInvest, slippage protection (10000 = 100% slippage allowed), 
+	# staking pool (4 for alUSD-3Crv on masterchef), asset number (0 alUSD, 1 DAI, 2 USDC, 3 USDT)
+    strategy = guardian.deploy(StrategyConvexCurveLP, vault, 1000000000 * (10 ** 18), 0, 10000, 4, 1)
+    strategy.setKeeper(keeper)
+    # lower the debtRatio of genlender to make room for our new strategy
+    vault.updateStrategyDebtRatio(genLender, 5000, {"from": gov})
+    vault.setManagementFee(0, {"from": gov})
+    curveVoterProxyStrategy.harvest({"from": liveGov})
+    vault.addStrategy(strategy, 1_200, 0, 0, 1000, {"from": gov})
+    yield strategy
 
 @pytest.fixture
 def vault(pm):
     Vault = pm(config["dependencies"][0]).Vault
     vault = Vault.at('0x27b7b1ad7288079A66d12350c828D3C00A6F07d7')
     yield vault
-
-@pytest.fixture
-def live_strategy(StrategyCurveIBVoterProxy):
-    live_strategy = StrategyCurveIBVoterProxy.at('0x5c0309fa022Bc1B73fE45A2D73EddeD58a820ff8')
-    yield live_strategy
-
-
-# @pytest.fixture
-# def crv3():
-#     yield Contract("0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490")
-#
-# @pytest.fixture
-# def usdc():
-#     yield Contract("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48")
-#
-# @pytest.fixture
-# def weth():
-#     yield Contract("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2")
-#
-# @pytest.fixture
-# def yveCrvContract(token):
-#     yield Contract("0xc5bDdf9843308380375a611c18B50Fb9341f502A")
-#
-# @pytest.fixture
-# def yveCrv(token):
-#     yield token
-#
-# @pytest.fixture
-# def whale_eth(accounts):
-#     yield accounts.at("0x73BCEb1Cd57C711feaC4224D062b0F6ff338501e", force=True)
-#
-# @pytest.fixture
-# def whale_3crv(accounts):
-#     yield accounts.at("0x5c00977a2002a3C9925dFDfb6815765F578a804f", force=True)
-#
-# @pytest.fixture
-# def sushiswap_crv(accounts):
-#     yield accounts.at("0x5c00977a2002a3C9925dFDfb6815765F578a804f", force=True)
-#
-# @pytest.fixture
-# def sushiswap(accounts):
-#     yield Contract("0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F")
--- tests/test_simple_harvest_and_tend.py
+++ tests/test_simple_harvest_and_tend.py
@@ -3,78 +3,62 @@ from brownie import Contract
 from brownie import config
 
 
-def test_simple_harvest_and_tend(gov, token, vault, dudesahn, strategist, whale, voter, strategyProxy, gaugeIB, rando, chain, amount, StrategyCurveIBVoterProxy, live_strategy, vault_balance, strategist_ms):
-    # Update deposit limit to the vault since it's currently maxed out
-    vault.setDepositLimit(100000000000000000000000000, {"from": strategist_ms})
+def test_simple_harvest_and_tend(gov, token, vault, dudesahn, strategist, whale, strategy, voter, gaugeIB, chain, strategist_ms):
+    ## deposit to the vault after approving
+    startingWhale = token.balanceOf(whale)
+    token.approve(vault, 2 ** 256 - 1, {"from": whale})
+    vault.deposit(1000e18, {"from": whale})
+    newWhale = token.balanceOf(whale)
     
-    # prepare our live strategy to migrate
-    vault.updateStrategyDebtRatio(live_strategy, 0, {"from": strategist_ms})
-    vault.revokeStrategy(live_strategy.address, {"from": strategist_ms})
-    live_strategy.harvest({"from": dudesahn})
-    
-    # assert that our old strategy is empty
-    assert live_strategy.estimatedTotalAssets() == 0
-
-    # deploy our new strategy
-    strategy = dudesahn.deploy(StrategyCurveIBVoterProxy, vault)
-
-    # migrate our old strategy
-    vault.migrateStrategy(live_strategy, strategy, {"from": strategist_ms})
-
-    # approve on new strategy with proxy
-    strategyProxy.approveStrategy(live_strategy.gauge(), strategy, {"from": gov})
-    vault.updateStrategyDebtRatio(strategy, 10000, {"from": strategist_ms})
-    strategy.harvest({"from": dudesahn})
-    startingVault = vault.totalAssets()
-    print("\nVault starting assets: ", startingVault)
-    
-    # assert new_strategy.estimatedTotalAssets() >= holdings
-    # assert that our old strategy is empty still
+    # harvest, store asset amount
+    strategy.harvest({"from": gov})
+    old_assets_dai = strategy.estimatedTotalAssets()
+    assert old_assets_dai > 0
     assert token.balanceOf(strategy) == 0
-    
+        
     # simulate a day of earnings
     chain.sleep(86400)
     chain.mine(1)
     
     # Test out our migrated strategy, confirm we're making a profit
-    strategy.harvest({"from": dudesahn})
+    strategy.harvest({"from": gov})
     assert strategy.tendCounter() == 0
-    vaultAssets_2 = vault.totalAssets()
-    assert vaultAssets_2 > startingVault
+    vaultAssets_2 = strategy.estimatedTotalAssets()
+    assert vaultAssets_2 > old_assets_dai
     print("\nAssets after 1 day harvest: ", vaultAssets_2)
     
-    ## for default migrations as a part of other tests, just copy all of the text above ##
+    staked_LP = masterchef.getStakeTotalDeposited(strategy, 4)
+    print("\nStaked LP: ", (staked_LP / 1e18))
+    assert masterchef.getStakeTotalDeposited(strategy, 4) > 0
 
-    # harvest, store asset amount
-    strategy.harvest({"from": dudesahn})
-    old_assets_dai = vault.totalAssets()
-    assert gaugeIB.balanceOf(voter) == strategyProxy.balanceOf(gaugeIB)
-    assert old_assets_dai >= strategyProxy.balanceOf(gaugeIB)
-    
-    # simulate a day of earnings
-    chain.sleep(86400)
-    chain.mine(1)
-        
-    # tend our strategy 
-    strategy.tend({"from": dudesahn})
-    
-    # simulate a month of earnings
-    chain.sleep(2592000)
-    chain.mine(1)
-
-    # harvest after a month, store new asset amount
-    tx = strategy.harvest({"from": dudesahn})
+    # harvest after a day, store new asset amount
+    strategy.harvest({"from": liveGov})
     # tx.call_trace(True)
-    new_assets_dai = vault.totalAssets()
+    new_assets_dai = strategy.estimatedTotalAssets()
     assert new_assets_dai > old_assets_dai
 
-    # Display estimated APR based on the past month
-    print("\nEstimated DAI APR: ", "{:.2%}".format(((new_assets_dai - old_assets_dai) * 12) / (old_assets_dai)))
+    # Display estimated APR based on the past day
+    print("\nEstimated DAI APR: ", "{:.2%}".format(((new_assets_dai - old_assets_dai) * 365) / (old_assets_dai)))
 
-    # wait to allow share price to reach full value (takes 6 hours as of 0.3.2)
-    chain.sleep(2592000)
-    chain.mine(1)
+    # we lose 0.04% whenever we deposit into a curve pool or withdraw
+    # withdraw whale's money and see if he's earned a profit (barely even with 0.08% loss)
+    vault.withdraw({"from": whale})    
+    print("This started as 1000 pool tokens", ( token.balanceOf(whale) - newWhale ) / 1e18)
+    assert token.balanceOf(whale) > startingWhale    staked_LP = masterchef.getStakeTotalDeposited(strategy, 4)
+    print("\nStaked LP: ", (staked_LP / 1e18))
+    assert masterchef.getStakeTotalDeposited(strategy, 4) > 0
 
-    # withdraw my money
-    vault.withdraw({"from": dudesahn})    
-    assert token.balanceOf(dudesahn) > 0 
+    # harvest after a day, store new asset amount
+    strategy.harvest({"from": liveGov})
+    # tx.call_trace(True)
+    new_assets_dai = strategy.estimatedTotalAssets()
+    assert new_assets_dai > old_assets_dai
+
+    # Display estimated APR based on the past day
+    print("\nEstimated DAI APR: ", "{:.2%}".format(((new_assets_dai - old_assets_dai) * 365) / (old_assets_dai)))
+
+    # we lose 0.04% whenever we deposit into a curve pool or withdraw
+    # withdraw whale's money and see if he's earned a profit (barely even with 0.08% loss)
+    vault.withdraw({"from": whale})    
+    print("This started as 1000 DAI", ( token.balanceOf(whale) - newWhale ) / 1e18)
+    assert token.balanceOf(whale) > startingWhale
