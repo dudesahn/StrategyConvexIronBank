6a788df4121c7632a8a5f58fe3c8b61ef8be1111
dudesahn <dudesahn@gmail.com>
2021-05-23 17:34:40 -0400

    fix: update revoke

--- tests/test_revoke.py
+++ tests/test_revoke.py
@@ -3,13 +3,16 @@ from brownie import Contract
 from brownie import config
 
 # test passes as of 21-05-20
-def test_revoke_strategy_from_vault(gov, token, vault, whale, chain, strategy, strat_setup):
+def test_revoke_strategy_from_vault(gov, token, vault, whale, chain, strategy):
     vaultAssets_starting = vault.totalAssets()
+    vault_holdings_starting = token.balanceOf(vault)
+    strategy_starting = strategy.estimatedTotalAssets()
     vault.revokeStrategy(strategy.address, {"from": gov})
     strategy.harvest({"from": gov})
     vaultAssets_after_revoke = vault.totalAssets()
     assert vaultAssets_after_revoke >= vaultAssets_starting
     assert strategy.estimatedTotalAssets() == 0
+    assert token.balanceOf(vault) >= vault_holdings_starting + strategy_starting
     
     # wait to allow share price to reach full value (takes 6 hours as of 0.3.2)
     chain.sleep(86400)
